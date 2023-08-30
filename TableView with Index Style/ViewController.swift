26689ead2c078449e611bf53ee86d08512217e8e
abdurrazzak0 <razzak6366@gmail.com>
2023-08-30 11:30:45 +0600

    Initial Commit

---
+++ TableView with Index Style/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView with Index Style
+//
+//  Created by Abdur Razzak on 30/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
